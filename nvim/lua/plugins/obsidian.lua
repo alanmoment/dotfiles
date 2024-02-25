48c48ab3dc169062f86c2b1ebace85db8627c673
alanchen <Alan.Chen@idtechproducts.com>
2024-02-25 14:42:40 +0800

    feat: add obsidian plugin

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,30 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  enabled = true,
+  ft = "markdown",
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+  --   "BufReadPre path/to/my-vault/**.md",
+  --   "BufNewFile path/to/my-vault/**.md",
+  -- },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "Obsidian",
+        path = "/Users/alan/Documents/Workspaces/devops/docs/Obsidian",
+      },
+    },
+
+    -- see below for full list of options 👇
+  },
+}
